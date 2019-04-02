13924262644f98571e43f579d639d79d2702cfa8
Asad Ullah <asad.yzai@gmail.com>
2019-04-02 23:44:44 +0500

    comments are added to the code

--- email.go
+++ email.go
@@ -13,30 +13,19 @@ import (
 )
 func main() {
 
-	//fmt.Println("Please Provide the Arguments in the sequence apikey=xxxxxx-xxxx-xxxx-xxxx-xxxxxx&statuses=4,5&from=2019-02-25T00:00:00&to=2019-03-03T23:59:59")
-	//argsWithProg := os.Args
-	//fmt.Println(argsWithProg)
-    //argsWithoutProg := os.Args[1:]
-    // API_KEY := os.Args[1]
-    // STATUSES := os.Args[2]
-    // D_FROM := os.Args[3]
-    // d_TO := os.Args[4]
-    API_KEY_Ptr := flag.String("apikey", "xxx-xxx-xxxx-xxxx-xxxx", "a string")
-    STATUSES_Ptr := flag.String("statuses", "x,y", "a string")
-    FROM_Ptr := flag.String("from", "2018-01-01:00:00:00", "a string")
-    TO_Ptr := flag.String("to", "2019-01-01:00:00:00", "a string")
+	// to take the input as a command parameters.
+    API_KEY_Ptr := flag.String("apikey", "apikey=xxx-xxx-xxxx-xxxx-xxxx", "a string")
+    STATUSES_Ptr := flag.String("statuses", "statuses=x,y or z", "a string")
+    FROM_Ptr := flag.String("from", "from=2018-01-01:00:00:00", "a string")
+    TO_Ptr := flag.String("to", "to=2019-01-01:00:00:00", "a string")
     flag.Parse()
-
-    // fmt.Println("apikey:", *API_KEY_Ptr)
-    // fmt.Println("statuses:", *STATUSES_Ptr)
-    // fmt.Println("from:", *FROM_Ptr)
-    // fmt.Println("to", *TO_Ptr)
 
     // fmt.Println(API_KEY, STATUSES, D_FROM, d_TO)
     
     //fmt.Println(argsWithoutProg)
     //fmt.Println(arg)
 
+    //to generate the URL from the taken inputs.
     u, err := url.Parse("http://bing.com/search")
 	if err != nil {
 		log.Fatal(err)
@@ -54,38 +43,36 @@ func main() {
 
 	//url := "https://api.elasticemail.com/v2/log/exportevents?apikey=3da0a60f-37a8-4363-bfd6-1cb0dbc525ee&statuses=4,5&from=2019-02-25T00:00:00&to=2019-03-03T23:59:59"
 
+	//creates an http client and hits the elastic email api and stores the JASON output. 
 	url := u.String()
-
 	req, _ := http.NewRequest("GET", url, nil)
-
 	res, _ := http.DefaultClient.Do(req)
-
 	defer res.Body.Close()
-
 	body, _ := ioutil.ReadAll(res.Body)
-
 	fmt.Println("The Status of the query is:")
-	
 	fmt.Println(string(body))
-
     myString := string(body)
     
+    //extracts the downloadable file url from the returned JASON.
     pat := regexp.MustCompile(`https?://.*\.csv`)
 	s := pat.FindString(myString)
 	fmt.Println("The extracted url is:")
 	fmt.Println(s)
 
 	//fileUrl := "https://api.elasticemail.com/userfile/55fa99ce-d2c7-41ad-9cad-e9d6476ba136/export/546dc7d5-bdf8-4842-917a-06b65092e347-eventslog.csv"
+	
+	//randers the downloadable file name with from and to dates.
 	fileUrl := s
 	DOWNLOADFILE_NAME := "eventslog-" + string(*FROM_Ptr) + "-" + string(*TO_Ptr)
     // if err := DownloadFile("email-events", fileUrl); err != nil {
+
+    	//calling a function that downloads the file, takes url as input and file name to be given
        if err := DownloadFile(DOWNLOADFILE_NAME, fileUrl); err != nil {
         panic(err)
     }
 }
 
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
+	//function that takes file path and url and downloads the file
 	func DownloadFile(filepath string, url string) error {
 
 	    // Get the data
